cf39c0d5733487f33b2afe136f636c3687d00b6c
degottardia <180385827+degottardia@users.noreply.github.com>
2025-01-15 08:37:36 +0000

    pulsante per prendere prima carta

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from modules.card import Card
 from modules.deck import Deck
+#from modules.card import Player 
 
 st.set_page_config(
    layout="wide",
@@ -20,8 +21,18 @@ st.image([card.image for card in deck.cards], width=card_width)
 st.markdown("## Shuffling deck")
 shuffle_button = st.button("Shuffle")
 if shuffle_button:
-    deck.shuffle()
+    shuffled_deck = deck.shuffle()
 st.image([card.image for card in deck.cards], width=card_width)
+
+cards= Card
+
+if st.button("Pesca carta"):
+    valore_mano= 0
+    carta_pescata = st.image([cards[0] for card in deck.cards])  # pescare la prima carta
+    valore_mano =+ card_scores  
+
+
+
 
 
 
