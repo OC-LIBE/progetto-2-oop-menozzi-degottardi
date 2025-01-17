a27916985368e94d45ae6f8b042000af21198f43
degottardia <180385827+degottardia@users.noreply.github.com>
2025-01-17 19:05:32 +0000

    aggiunte funzioni player, ne mancano ancora e vanno controllate

--- modules/player.py
+++ modules/player.py
@@ -1,11 +1,26 @@
 import random
 from modules.card import Card
 
-
 class Player:
-    def __init__(self, hand_value, money):
-        self.hand_value = hand_value
-        self.money= money
+    def __init__(self, name):
+        self.name = name #nome esterno o il banco
+        self.hand = [] #lista carte in mano
+        self.scores= [] #lista  punteggi  
+
+    def aggiungi_carta(self, card):
+        self.hand.append(card) #aggiungo la carta alla mnao
+        self.scores= self.calcolo_punteggi() #per calcolare i nuovi punteggi
+
+    def calcolo_punteggi(self): #da rivedere come  salvare tutti i risultati possibili
+        scores = [0]
+        for card in self.hand:
+            new_scores = []
+            for score in scores:
+                for card_score in card.cards_scores: #devo valutare anche le varie possibilita ddell'asse 
+                    new_scores.append(score + card_score)
+            scores = #new scores in una lista, come??
+
+    #mancano funzioni per vedere se hai perso o hai vinto (< o > 21)
 
     def draw(self):
         if len(self.cards) == 0:
@@ -14,7 +29,3 @@ class Player:
         self.cards.remove(self.cards[0])
         print(len(self.cards))
         return drawn_card
-
-    def aggiorna_mano(self):
-        mano = 0 
-        mano =+  self.cards_scores 
