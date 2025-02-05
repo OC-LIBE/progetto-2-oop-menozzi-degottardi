611958be9f223717fca6e3043de42dfb858e228f
degottardia <180385827+degottardia@users.noreply.github.com>
2025-02-05 07:46:31 +0000

    hand

--- modules/hand.py
+++ modules/hand.py
@@ -4,7 +4,17 @@ from modules.card import Card
 class Hand:
     def __init__(self, cards, score):
         self.cards= []
-        self.score= []
+        self.score= 0
     
-    def add_card():
+    def add_card(self, card):
+        self.cards.append(card)
+    
+    def calcola_score(self, score):
+        scores= 0
+        for card in self.hand:
+            new_scores= []
+
+    def discard(self):
+        self.cards.clear()
+        self.score = 0
         
