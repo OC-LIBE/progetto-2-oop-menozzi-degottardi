3e55bfde4f68b173d1f6713aad87facf359f79fc
degottardia <180385827+degottardia@users.noreply.github.com>
2025-02-05 07:26:49 +0000

    creazione oggetto hand

---
+++ modules/hand.py
@@ -0,0 +1,10 @@
+import random
+from modules.card import Card 
+
+class Hand:
+    def __init__(self, cards, score):
+        self.cards= []
+        self.score= []
+    
+    def add_card():
+        
