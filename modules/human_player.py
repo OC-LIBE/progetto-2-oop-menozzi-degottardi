8463bb18c654bfb28c7cb79fd4ec3c4d61a63ce3
dmenozzi <dmenozzi@bluewin.ch>
2025-02-12 08:13:20 +0000

    ritocco oggetti

--- modules/human_player.py
+++ modules/human_player.py
@@ -6,3 +6,10 @@ class HumanPlayer(Player):
     def __init__(self, chips, last_score):
         self.chips= chips
         self.last_score= last_score
+
+    def place_bet(self, scommessa):
+        if amount <= self.chips:
+            self.chips -= amount 
+            return amount 
+        else:
+            return 0
