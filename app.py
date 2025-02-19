637654cc254d199fb456d0993e2624f39218e9ff
degottardia <180385827+degottardia@users.noreply.github.com>
2025-02-19 08:20:47 +0000

    importare game nell'app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from modules.card import Card
 from modules.deck import Deck
+from modules.game import Game
 
 st.set_page_config(
    layout="wide",
 )
 card_width=95
-
 
 number_of_decks = st.number_input("Number of decks", min_value=1, max_value=10, value=1)
 
@@ -28,3 +28,10 @@ pesca_button = st.button("Pesca una carta")
 if pesca_button: 
    deck.draw()
 st.image([card.image for card in deck.cards], width=card_width)
+
+
+
+
+
+
+
