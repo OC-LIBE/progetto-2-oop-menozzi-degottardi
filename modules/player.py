3593a170a35b1b2b6ace9773753f919656dd7892
degottardia <180385827+degottardia@users.noreply.github.com>
2025-02-19 08:21:18 +0000

    -

--- modules/player.py
+++ modules/player.py
@@ -1,11 +1,11 @@
 import random
-from modules.hand import hand 
+from modules.hand import Hand 
 
 class Player:
     def __init__(self, name, avatar):
         self.name= name
         self.avatar= avatar
-        self.hand= Hand[]
+        self.hand= Hand
         self.chips = 100 #chips iniziali 
 
     
